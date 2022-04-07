ebfc7195f0c2c1ef95eaac42d545a33c87877d6e
Divya872 <102275435+Divya872@users.noreply.github.com>
2022-04-07 11:26:18 +0530

    Add files via upload

---
+++ userDetails.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+require('dotenv').config();
+
+// Connecting Mongoose
+mongoose.connect("mongodb://localhost:27017/database " ,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() =>
+    console.log("establish connection")
+)
+.catch ((err)=>console.log(err))
+
+// Setting up the schema
+const User = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+// Setting up the passport plugin
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
