08e78bb1bf9bad20ca5c48b03a09b32451dd8459
Rashi gupta <87823367+Rashi-123-gupta@users.noreply.github.com>
2022-04-07 12:45:43 +0530

    modified

--- userDetails.js
+++ userDetails.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 require('dotenv').config();
@@ -21,28 +21,5 @@ const User = new mongoose.Schema({
 // Setting up the passport plugin
 User.plugin(passportLocalMongoose);
 
-=======
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
-require('dotenv').config();
 
-// Connecting Mongoose
-mongoose.connect("mongodb://localhost:27017/database " ,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() =>
-    console.log("establish connection")
-)
-.catch ((err)=>console.log(err))
-
-// Setting up the schema
-const User = new mongoose.Schema({
-  username: String,
-  password: String,
-});
-
-// Setting up the passport plugin
-User.plugin(passportLocalMongoose);
-
->>>>>>> 3408ef444c56f8a04215cd89432f14d1a5eaa089
 module.exports = mongoose.model('User', User);
